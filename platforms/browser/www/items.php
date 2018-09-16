2c6c1e43ce2fd5301cc113a87811fcf89cc0735d
sibox1337 <sebo1337@gmail.com>
2018-09-17 00:19:42 +0300

    modified:   platforms/android/app/src/main/assets/www/groups.html modified:   platforms/android/app/src/main/assets/www/items.html modified:   platforms/android/app/src/main/assets/www/items.php modified:   platforms/android/app/src/main/assets/www/library.php modified:   platforms/browser/www/groups.html modified:   platforms/browser/www/items.html modified:   platforms/browser/www/items.php modified:   platforms/browser/www/library.php modified:   www/groups.html modified:   www/items.html modified:   www/items.php modified:   www/library.php

--- platforms/browser/www/items.php
+++ platforms/browser/www/items.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 require __DIR__ .'/library.php';
@@ -10,16 +9,3 @@ $task = new Panorama();
 
 echo $task->getItems($_GET['grid']);
 
-=======
-<?php
-
-require __DIR__ .'/library.php';
-
-$task = new Panorama();
-
-
-
-
-echo $task->getItems($_GET['grid']);
-
->>>>>>> fb8818c3c529d5503704a640d3948645e195b14a
--- www/library.php
+++ www/library.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 
@@ -55,62 +54,4 @@ class Panorama
 	}
 	
 	
-=======
-<?php
-
-
-require __DIR__ .'/db_connection.php';
-
-
-
-
-class Panorama
-{
-	
-	protected $db;
-	
-	
-	public function __construct() {
-		
-		$this->db = DB();		
-		
-	}
-	
-	
-	public function getGroups() {
-		$query = $this->db->query('SELECT * FROM GRUP WHERE CHAR_LENGTH(IMEGR) > 2 ORDER BY ID_GR ASC ');
-		$query->execute();
-		$data = array();
-		
-		while($row = $query->fetch(PDO::FETCH_ASSOC)) {
-			
-		
-		$data[] = mb_convert_encoding($row,'utf-8','windows-1251');
-		
-		}
-		
-	
-	
-	return json_encode(['groups' => $data]);
-	
-	}
-	
-	public function getItems($id) {
-		
-		$query = $this->db->query('SELECT * FROM ART WHERE GR_ART ='.$id.' AND CENA_ART > 0');
-		$query->execute();
-		$data = array();
-		while($row = $query->fetch(PDO::FETCH_ASSOC)) {
-			
-			
-			$data[] = mb_convert_encoding($row,'utf-8','windows-1251');
-		}
-		
-		return json_encode(['items'	=> $data]);
-
-		
-	}
-	
-	
->>>>>>> fb8818c3c529d5503704a640d3948645e195b14a
 }
